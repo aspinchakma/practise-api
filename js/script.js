c5353e2c2f6c7ca9c8cc63fe881867a076272283
aspinchakma <85893728+aspinchakma@users.noreply.github.com>
2021-09-01 17:27:57 +0600

    ok

---
+++ js/script.js
@@ -0,0 +1,87 @@
+const inputButton = document.getElementById('input-button');
+const inputField = document.getElementById('input-field');
+const itemsContainer = document.getElementById('items-container');
+const spinners = document.getElementById('spinner-display');
+const emptyInput = document.getElementById('empty-input');
+const invalidName = document.getElementById('invalid-name');
+
+
+
+const getAllResult = () => {
+    const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s`;
+    fetch(url)
+        .then(response => response.json())
+        .then(data => displayResult(data.drinks))
+}
+getAllResult();
+
+const displayResult = (items) => {
+    for (const item of items) {
+        const div = document.createElement('div');
+        div.className = 'col';
+        div.innerHTML = `<div class="card" style="cursor: pointer;" >
+        <img src="${item.strDrinkThumb}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title text-center">${item.strDrink}</h5>
+        </div>
+    </div>`;
+
+        itemsContainer.appendChild(div);
+    }
+}
+
+
+
+const spinnerToggles = (display) => {
+    spinners.style.display = `${display}`
+}
+const invalidNameWarr = (display) => {
+    invalidName.style.display = `${display}`
+}
+
+inputButton.addEventListener('click', () => {
+    const inputValue = inputField.value;
+    itemsContainer.textContent = '';
+    if (inputValue.length === 0) {
+        emptyInput.style.display = 'block';
+        invalidNameWarr('none')
+    } else {
+        getData(inputValue);
+        spinnerToggles('block');
+        emptyInput.style.display = 'none';
+        invalidNameWarr('none')
+    }
+    // getData(inputValue)
+    inputField.value = '';
+    // spinnerToggles('block')
+});
+
+
+const getData = (searchText) => {
+    const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchText}`;
+    fetch(url)
+        .then(response => response.json())
+        .then(data => getItem(data.drinks))
+};
+
+const getItem = (itemName) => {
+    if (itemName !== null) {
+        itemName.forEach(pro => {
+            const div = document.createElement('div');
+            div.className = 'col';
+            div.innerHTML = `<div class="card" style="cursor: pointer;" >
+        <img src="${pro.strDrinkThumb}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title text-center">${pro.strDrink}</h5>
+        </div>
+    </div>`;
+
+            itemsContainer.appendChild(div);
+        });
+        spinnerToggles('none')
+    } else {
+        spinnerToggles('none');
+        console.log(invalidName)
+        invalidNameWarr('block')
+    }
+}
